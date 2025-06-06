b8790e234f55cd790d6316e5a6f84c485bddcba3
Jose Pacelli Moreira de Oliveira <jose.pacelli@atlantatecnologia.com.br>
2025-06-06 11:28:39 -0300

    gitignore

--- build-linux.sh
+++ build-linux.sh
@@ -8,5 +8,4 @@ docker buildx inspect mybuilder --bootstrap
 docker buildx build --pull --no-cache --platform linux/amd64,linux/arm64 \
   -f Dockerfile \
   -t atlantatecnologia/at-balanca-haenni-simulador:latest \
-  -t atlantatecnologia/at-balanca-haenni-simulador:arm64 \
   --push .
